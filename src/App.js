6677262201858124ae3ae143a04a772bf7dd89b9
Megha Niharika H S <megha.hs@BRILLIO.COM>
2019-09-18 13:03:00 +0530

    added date class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom"
 
-function App() {
+class App extends React.Component{
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+     <Header/>
+     <Greeting/>
+      </div>
+  )
 }
 
+  
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  // );
+  
+}
+
+class Header extends React.Component{
+
+render(props)
+{
+  return(
+    <div>
+      <h1>welcome!{this.props.username}</h1>
+      </div>
+
+  )
+}
+}
+
+
+
+
+class  Greeting extends React.Component{
+render()
+{
+  const date = new Date()
+  const Hours =  date.getHours()
+  let timeday
+
+  if(Hours < 12){
+    timeday="morning"
+  }
+  else if(Hours >= 12 && Hours < 17)
+  {
+    timeday ="afternun"
+
+  }
+  else{
+    timeday = "night"
+  }
+return(
+  <h1>{timeday} to you</h1>
+)
+
+}
+}
 export default App;
